7b4ed3a62c9125be11ce252edb221cda7c0065ea
ipko1996 <ipko1996@gmail.com>
2022-11-03 22:58:08 +0100

    char bugfix

--- app/page.tsx
+++ app/page.tsx
@@ -14,8 +14,10 @@ export default function Home() {
         <h3 className="text-slate-900 dark:text-white mt-5 text-base font-medium tracking-tight">
           Work in progress...
         </h3>
-        <p className={`${roboto_mono.className} text-slate-500 dark:text-slate-400 mt-2 text-sm`}>
-          >COMING: WHEN IT'S READY
+        <p
+          className={`${roboto_mono.className} text-slate-500 dark:text-slate-400 mt-2 text-sm`}
+        >
+          &#62;COMING: WHEN IT'S READY
         </p>
       </div>
     </div>
